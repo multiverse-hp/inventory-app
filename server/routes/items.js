b2f1572222d5d3f44cd5e8b16d3665d9432d7264
sunnidaze7 <pnykii98@gmail.com>
2023-07-17 16:02:36 -0500

    merged delete and get request

--- server/routes/items.js
+++ server/routes/items.js
@@ -32,4 +32,30 @@ router.get("/:id", async (req, res, next) =>{
     }
 });
 
+// DELETE /items
+router.delete("/", async (req, res, next) => {
+  try {
+    await Item.destroy({ where: {} });
+    res.send("All items have been deleted");
+  } catch (error) {
+    next(error);
+  }
+});
+
+// DELETE /items/:itemID
+router.delete("/:itemId", async (req, res, next) => {
+  try {
+    const itemID = req.params.itemID;
+    const item = await Item.findByPk(itemId);
+    if (!item) {
+      res.status(400).send("Item not found");
+    } else {
+      await item.destroy();
+      res.send("Item has been deleted");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
