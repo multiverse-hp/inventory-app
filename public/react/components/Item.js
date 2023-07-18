1d05f24f4507dd00afb5115895e8c1faf526eeda
sunnidaze7 <pnykii98@gmail.com>
2023-07-18 13:28:04 -0500

    front end view of all items

---
+++ public/react/components/Item.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import apiURL from '../api';
+
+export const Item = (props) => {
+
+  // async function getItem(){
+  //   let itemData = await fetch(`${apiURL}/items/${props.item.id}`)
+  //   let item = await itemData.json()
+  //   props.setItems(item)
+  // }
+
+  return( 
+  <div className = 'item'>
+    <h3>{props.item.title}</h3>
+    <h3>{props.item.description}</h3>
+    <h3>{props.item.price}</h3>
+    <h3>{props.item.category}</h3>
+    <img 
+    // onClick={getItem} 
+    src={props.item.image} alt={props.item.name} />
+  </div>
+  )
+}
+
+module.exports = { Item }
---
+++ server/models/Item.js
@@ -0,0 +1,16 @@
+// const {Sequelize} = require('sequelize')
+// const {sequelize} = require('../db')
+
+
+
+// const Item = sequelize.define("items", {
+//     title: Sequelize.STRING,
+//     description: Sequelize.STRING,
+//     price: Sequelize.FLOAT,
+//     category: Sequelize.STRING,
+//     image: Sequelize.TEXT,
+//   });
+//   module.exports = {
+//     db: sequelize,
+//     Item
+//   };
