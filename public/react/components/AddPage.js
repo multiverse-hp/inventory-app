2ca201d4a3b43afd1acc5332b52d840005cc2959
Johnny <aguguaj@hotmail.com>
2023-07-19 11:00:17 -0500

    Added some logic to AddPage comp

--- public/react/components/AddPage.js
+++ public/react/components/AddPage.js
@@ -7,11 +7,31 @@ import apiURL from '../api';
 export const AddPage = (props) => {
 
   
-  }
+  
 
   return( 
     <>
-    
+    <div>
+        <h1 className="text-center">Add Page</h1><br/>
+        <form onSubmit={handleSubmit}>
+            <label>Title:</label>
+            <input type='text' id='title' name='title'></input>
+
+            <label>Description:</label>
+            <textarea rows='4' cols='50' placeholder='Enter description here...' required></textarea>
+
+
+            <label>Category:</label>
+            <textarea rows='4' cols='50' placeholder='Enter category here...' required></textarea>
+
+
+            <label>Price:</label>
+            <input type='number' min= "0" step=".01"></input>
+
+            <button type='submit'>Save Changes</button>
+
+        </form>
+    </div>
     </>
   
   )
