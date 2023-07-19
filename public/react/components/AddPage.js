fb81756521e3c5a85fb1796853b47941e072acc5
Johnny <aguguaj@hotmail.com>
2023-07-19 10:48:02 -0500

    Created addpage comp

---
+++ public/react/components/AddPage.js
@@ -0,0 +1,20 @@
+import React, { useState,useEffect } from 'react';
+import apiURL from '../api';
+
+// import '../../../public/style.css'
+
+
+export const AddPage = (props) => {
+
+  
+  }
+
+  return( 
+    <>
+    
+    </>
+  
+  )
+}
+
+module.exports = { Item }
