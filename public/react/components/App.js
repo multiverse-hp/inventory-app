1f9440e7293d8b6e22c27480c4f1a2cb1396c675
Johnny <aguguaj@hotmail.com>
2023-07-20 10:05:47 -0500

    stable

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { SaucesList } from './SaucesList';
 import { ItemsList } from './ItemsList';
 import { ItemDetails } from './ItemDetails';
-
 import { AddPage } from './AddPage';
  
 // import and prepend the api url to any fetch calls
@@ -13,7 +12,7 @@ export const App = () => {
 	// const [sauces, setSauces] = useState([]);
 	//intialized items state
 	const [items, setItems] = useState([])
-    const [clickItems, setClickItems] = useState(null);   
+    const [clickItems, setClickItems] = useState(null);
 	// const [ query, setQuery ] = useState('');
 	const [ showAddPage, setShowAddPage ] = useState(false);
 
@@ -21,7 +20,7 @@ export const App = () => {
 	// async function fetchSauces(){
 	// 	try {
 	// 		const response = await fetch(`${apiURL}/sauces`);
-	// 		const saucesData = await response.json();  
+	// 		const saucesData = await response.json();
 			
 	// 		console.log(saucesData);
 	// 		setSauces(saucesData)
@@ -71,39 +70,10 @@ export const App = () => {
 			<h1>Inventory</h1>
 			<h2>All items</h2>
 			<button onClick={handleAddItemClick}>Add Item</button>
-
-// 			{/* {showAddPage ? (
-// 				<AddPage setItems={setItems} setShowAddPage={setShowAddPage} items={items}/> ) : <button onClick={handleAddItemClick}>Add Item</button>
-// 			} */}
-// 			{clickItems !== null ? <ItemDetails item={clickItems} setClickItems={setClickItems} setItems={setItems} items={items}/> : 
-// 			<div>
-// 				{/* <div>
-// 				<label>Search</label>
-// 				<input type="text" placeholder='search' onChange={(e)=>setQuery(e.target.value)} />
-// 				</div> */}
-// 			<div className='title-div'>
-// 			<h1>Inventory</h1>
-// 			<h2>All items</h2>
-
-// 			{/* button to add item */}
-
-			
-// 			{/* <button>Add Item</button> */}
-// 			{showAddPage ? (
-// 				<AddPage setItems={setItems} setShowAddPage={setShowAddPage} items={items}/> ) : <button onClick={handleAddItemClick}>Add Item</button>
-// 			}
-// 			</div>
-		
-
-// 			{/* <button onClick={handleAddItemClick}>Add Item</button> */}
-
 			<ItemsList item={clickItems} setClickItems={setClickItems} setItems={setItems} items={items}/>
 			</div> 
 			)}
 			
 		</main>
 	)
-}	
-
-  
-  
+}
